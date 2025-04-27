66f8eb25d1ad59b9b01609dc66a0af6c47f5d82d
GIo-RIvera <50181811+riveragio@users.noreply.github.com>
2025-04-27 20:19:51 +0800

    Add files via upload

---
+++ math.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(){
+
+
+
+    return 0;
+}
+
+/*
+
+Square Root
+sqrt(num)
+
+Round Number
+ceil(num)
+floor(num)
+
+Power
+pow(4, 3) 4^3
+
+*/
---
+++ string.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(){
+
+
+
+    return 0;
+}
+
+/*
+
+String Concatenate
+strcat(str1, str2);
+
+String Concatenate with specified number
+strncat(str2, str1, 5);
+
+String Compare
+strcmp(str1, str2);
+
+String Copy
+strcpy(str1, str2);
+
+String Length
+strlen(str1, str2);
+
+String Reverse
+strrev(str);
+
+String Lower
+strlwr(str)
+
+String Upper
+strupr(str)
+
+*/
